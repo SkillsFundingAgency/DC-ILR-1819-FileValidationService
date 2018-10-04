ee86a127ba2f1d54da28d57b09a4191be9e8c290
Mike Childs <mike@mjcdev.co.uk>
2018-10-04 16:26:05 +0100

    #61067 - Rudimentary Mapper and half baked test, needs refinement with large, varied, ILR file

---
+++ src/ESFA.DC.ILR.FileValidationService.Service/Interface/ILooseToTightSchemaMapper.cs
@@ -0,0 +1,9 @@
+﻿using ESFA.DC.ILR.Model;
+
+namespace ESFA.DC.ILR.FileValidationService.Service.Interface
+{
+    public interface ILooseToTightSchemaMapper
+    {
+        Message Map(Model.Loose.Message looseMessage);
+    }
+}
---
+++ src/ESFA.DC.ILR.FileValidationService.Service/LooseToTightSchemaMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using ESFA.DC.ILR.FileValidationService.Service.Interface;
+
+namespace ESFA.DC.ILR.FileValidationService.Service
+{
+    public class LooseToTightSchemaMapper : ILooseToTightSchemaMapper
+    {
+        public Model.Message Map(Model.Loose.Message looseMessage)
+        {
+            Mapper.Initialize(c => c.CreateMap<Model.Loose.Message, Model.Message>());
+
+            var output = Mapper.Map<Model.Loose.Message, Model.Message>(looseMessage);
+
+            return output;
+        }
+    }
+}
