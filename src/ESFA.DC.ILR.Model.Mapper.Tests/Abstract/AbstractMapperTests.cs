7138830b4e9ea94b16b60d7ab2af054915cb9d93
Mike Childs <mike@mjcdev.co.uk>
2018-11-20 13:21:15 +0000

    #61067 - Source Mapper

--- src/ESFA.DC.ILR.Model.Mapper.Tests/Abstract/AbstractMapperTests.cs
+++ src/ESFA.DC.ILR.Model.Mapper.Tests/Abstract/AbstractMapperTests.cs
@@ -14,7 +14,9 @@ namespace ESFA.DC.ILR.Model.Mapper.Tests.Abstract
         where TOut : new()
     {
         protected readonly DateTime TestDateTime = new DateTime(2018, 1, 1);
-
+        protected const string TestString = "TestString";
+        protected const int TestInt = 1234567;
+        
         protected void TestMapperProperty<TPropertyIn, TPropertyOut>(IModelMapper<TIn, TOut> mapper, Expression<Func<TIn, TPropertyIn>> inputSelector, TPropertyIn inputValue, Expression<Func<TOut, TPropertyOut>> outputSelector, TPropertyOut outputValue)
         {
             var inputModel = NewInputModelEntity(inputSelector, inputValue);
---
+++ src/ESFA.DC.ILR.Model.Mapper/SourceMapper.cs
@@ -0,0 +1,23 @@
+﻿using ESFA.DC.ILR.Model.Mapper.Abstract;
+
+namespace ESFA.DC.ILR.Model.Mapper
+{
+    public class SourceMapper : AbstractModelMapper<Loose.MessageHeaderSource, MessageHeaderSource>
+    {
+        protected override MessageHeaderSource MapModel(Loose.MessageHeaderSource model)
+        {
+            return new MessageHeaderSource()
+            {
+                ComponentSetVersion = model.ComponentSetVersion,
+                DateTime = model.DateTime,
+                ProtectiveMarking = (MessageHeaderSourceProtectiveMarking)model.ProtectiveMarking,
+                ReferenceData = model.ReferenceData,
+                Release = model.Release,
+                SerialNo = model.SerialNo,
+                SoftwarePackage = model.SoftwarePackage,
+                SoftwareSupplier = model.SoftwareSupplier,
+                UKPRN = model.UKPRN,
+            };
+        }
+    }
+}
