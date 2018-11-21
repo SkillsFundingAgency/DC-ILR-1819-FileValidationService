55b7bb507085f4738fbc92b63eaf75869685e396
Mike Childs <mike@mjcdev.co.uk>
2018-11-21 09:55:39 +0000

    #61067 - Learner HE Mapper

--- src/ESFA.DC.ILR.FileValidationService.Modules/MapperModule.cs
+++ src/ESFA.DC.ILR.FileValidationService.Modules/MapperModule.cs
@@ -20,6 +20,7 @@ namespace ESFA.DC.ILR.FileValidationService.Modules
             containerBuilder.RegisterType<LLDDAndHealthProblemMapper>().As<IModelMapper<Model.Loose.MessageLearnerLLDDandHealthProblem, MessageLearnerLLDDandHealthProblem>>();
             containerBuilder.RegisterType<LearnerEmploymentStatusMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerEmploymentStatus, MessageLearnerLearnerEmploymentStatus>>();
             containerBuilder.RegisterType<LearnerFAMMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerFAM, MessageLearnerLearnerFAM>>();
+            containerBuilder.RegisterType<LearnerHEMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerHE, MessageLearnerLearnerHE>>();
         }
     }
 }
---
+++ src/ESFA.DC.ILR.Model.Mapper/LearnerHEMapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using ESFA.DC.ILR.Model.Mapper.Abstract;
+using ESFA.DC.ILR.Model.Mapper.Interface;
+
+namespace ESFA.DC.ILR.Model.Mapper
+{
+    public class LearnerHEMapper : AbstractModelMapper<Loose.MessageLearnerLearnerHE, MessageLearnerLearnerHE>
+    {
+        private readonly IModelMapper<Loose.MessageLearnerLearnerHELearnerHEFinancialSupport, MessageLearnerLearnerHELearnerHEFinancialSupport> _learnerHeFinancialSupportMapper;
+
+        public LearnerHEMapper(IModelMapper<Loose.MessageLearnerLearnerHELearnerHEFinancialSupport, MessageLearnerLearnerHELearnerHEFinancialSupport> learnerHEFinancialSupportMapper)
+        {
+            _learnerHeFinancialSupportMapper = learnerHEFinancialSupportMapper;
+        }
+
+        protected override MessageLearnerLearnerHE MapModel(Loose.MessageLearnerLearnerHE model)
+        {
+            return new MessageLearnerLearnerHE()
+            {
+                LearnerHEFinancialSupport = model.LearnerHEFinancialSupport?.Select(s => _learnerHeFinancialSupportMapper.Map(s)).ToArray(),
+                TTACCOM = (int)model.TTACCOM,
+                TTACCOMSpecified = model.TTACCOMSpecified,
+                UCASPERID = model.UCASPERID,
+            };
+        }
+    }
+}
