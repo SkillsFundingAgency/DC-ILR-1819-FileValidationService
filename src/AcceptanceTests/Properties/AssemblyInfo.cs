995159cd2cd59494f29d3b344aeffb57a0926517
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-11-30 15:09:42 +0000

    65463 : Add Basic SpecFlow Acceptance Tests to check that Build still work and Acceptance Tests are only run after deployment.

---
+++ src/AcceptanceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ESFA.DC.ILR.FileValidationService.AcceptanceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ESFA.DC.ILR.FileValidationService.AcceptanceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("710a5cf8-0916-4c54-ab3f-61dc0c749bf4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
