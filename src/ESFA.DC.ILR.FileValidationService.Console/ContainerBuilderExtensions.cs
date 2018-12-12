45f3f5b6954d316f385c46fd4321dffaed76540a
Mike Childs <mike@mjcdev.co.uk>
2018-12-12 10:49:28 +0000

    Resurrected the Console App for Testing, added If Statement around error validity for LearnerDestinationAndProgressions

--- src/ESFA.DC.ILR.FileValidationService.Console/ContainerBuilderExtensions.cs
+++ src/ESFA.DC.ILR.FileValidationService.Console/ContainerBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using ESFA.DC.FileService;
 using ESFA.DC.FileService.Interface;
+using ESFA.DC.ILR.FileValidationService.Console.Stubs;
 using ESFA.DC.ILR.FileValidationService.Rules;
 using ESFA.DC.ILR.FileValidationService.Service;
 using ESFA.DC.ILR.FileValidationService.Service.Interface;
@@ -8,8 +9,11 @@ using ESFA.DC.ILR.Model;
 using ESFA.DC.ILR.Model.Loose.Interface;
 using ESFA.DC.ILR.Model.Loose.Schema;
 using ESFA.DC.ILR.Model.Loose.Schema.Interface;
+using ESFA.DC.ILR.Model.Mapper;
+using ESFA.DC.ILR.Model.Mapper.Interface;
 using ESFA.DC.IO.Dictionary;
 using ESFA.DC.IO.Interfaces;
+using ESFA.DC.Logging.Interfaces;
 using ESFA.DC.Mapping.Interface;
 using ESFA.DC.Serialization.Interfaces;
 using ESFA.DC.Serialization.Json;
@@ -56,6 +60,8 @@ namespace ESFA.DC.ILR.FileValidationService.Console
             containerBuilder.RegisterType<FileSystemFileService>().As<IFileService>();
             containerBuilder.RegisterType<DictionaryKeyValuePersistenceService>().As<IKeyValuePersistenceService>();
             containerBuilder.RegisterType<StronglyTypedKeyValuePersistenceService>().As<IStronglyTypedKeyValuePersistenceService>();
+            containerBuilder.RegisterType<FileValidationPreparationServiceStub>().As<IFileValidationPreparationService>();
+            containerBuilder.RegisterType<FileValidationIOModelBuilder>().As<IFileValidationIOModelBuilder>();
 
             return containerBuilder;
         }
@@ -72,6 +78,42 @@ namespace ESFA.DC.ILR.FileValidationService.Console
             containerBuilder.RegisterType<LooseToTightSchemaMapper>().As<IMapper<Model.Loose.Message, Message>>();
             containerBuilder.RegisterType<FileValidationOutputService>().As<IFileValidationOutputService>();
             containerBuilder.RegisterType<FileValidationOrchestrationService>().As<IFileValidationOrchestrationService>();
+
+            return containerBuilder;
+        }
+
+        public static ContainerBuilder RegisterMappers(this ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<MessageMapper>().As<IModelMapper<Model.Loose.Message, Message>>();
+            containerBuilder.RegisterType<HeaderMapper>().As<IModelMapper<Model.Loose.MessageHeader, MessageHeader>>();
+            containerBuilder.RegisterType<CollectionDetailsMapper>().As<IModelMapper<Model.Loose.MessageHeaderCollectionDetails, MessageHeaderCollectionDetails>>();
+            containerBuilder.RegisterType<SourceMapper>().As<IModelMapper<Model.Loose.MessageHeaderSource, MessageHeaderSource>>();
+            containerBuilder.RegisterType<LearningProviderMapper>().As<IModelMapper<Model.Loose.MessageLearningProvider, MessageLearningProvider>>();
+            containerBuilder.RegisterType<SourceFileMapper>().As<IModelMapper<Model.Loose.MessageSourceFile, MessageSourceFile>>();
+            containerBuilder.RegisterType<LearnerMapper>().As<IModelMapper<Model.Loose.MessageLearner, MessageLearner>>();
+            containerBuilder.RegisterType<ContactPreferenceMapper>().As<IModelMapper<Model.Loose.MessageLearnerContactPreference, MessageLearnerContactPreference>>();
+            containerBuilder.RegisterType<LLDDAndHealthProblemMapper>().As<IModelMapper<Model.Loose.MessageLearnerLLDDandHealthProblem, MessageLearnerLLDDandHealthProblem>>();
+            containerBuilder.RegisterType<LearnerEmploymentStatusMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerEmploymentStatus, MessageLearnerLearnerEmploymentStatus>>();
+            containerBuilder.RegisterType<LearnerFAMMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerFAM, MessageLearnerLearnerFAM>>();
+            containerBuilder.RegisterType<LearnerHEMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerHE, MessageLearnerLearnerHE>>();
+            containerBuilder.RegisterType<ProviderSpecLearnerMonitoringMapper>().As<IModelMapper<Model.Loose.MessageLearnerProviderSpecLearnerMonitoring, MessageLearnerProviderSpecLearnerMonitoring>>();
+            containerBuilder.RegisterType<LearningDeliveryMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearningDelivery, MessageLearnerLearningDelivery>>();
+            containerBuilder.RegisterType<EmploymentStatusMonitoringMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerEmploymentStatusEmploymentStatusMonitoring, MessageLearnerLearnerEmploymentStatusEmploymentStatusMonitoring>>();
+            containerBuilder.RegisterType<LearnerHEFinancialSupportMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearnerHELearnerHEFinancialSupport, MessageLearnerLearnerHELearnerHEFinancialSupport>>();
+            containerBuilder.RegisterType<AppFinRecordMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearningDeliveryAppFinRecord, MessageLearnerLearningDeliveryAppFinRecord>>();
+            containerBuilder.RegisterType<LearningDeliveryFAMMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearningDeliveryLearningDeliveryFAM, MessageLearnerLearningDeliveryLearningDeliveryFAM>>();
+            containerBuilder.RegisterType<LearningDeliveryHEMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearningDeliveryLearningDeliveryHE, MessageLearnerLearningDeliveryLearningDeliveryHE>>();
+            containerBuilder.RegisterType<LearningDeliveryWorkPlacementMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearningDeliveryLearningDeliveryWorkPlacement, MessageLearnerLearningDeliveryLearningDeliveryWorkPlacement>>();
+            containerBuilder.RegisterType<ProviderSpecLearningDeliveryMonitoringMapper>().As<IModelMapper<Model.Loose.MessageLearnerLearningDeliveryProviderSpecDeliveryMonitoring, MessageLearnerLearningDeliveryProviderSpecDeliveryMonitoring>>();
+            containerBuilder.RegisterType<LearnerDestinationAndProgressionMapper>().As<IModelMapper<Model.Loose.MessageLearnerDestinationandProgression, MessageLearnerDestinationandProgression>>();
+            containerBuilder.RegisterType<DPOutcomeMapper>().As<IModelMapper<Model.Loose.MessageLearnerDestinationandProgressionDPOutcome, MessageLearnerDestinationandProgressionDPOutcome>>();
+
+            return containerBuilder;
+        }
+
+        public static ContainerBuilder RegisterLogger(this ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<LoggerStub>().As<ILogger>();
 
             return containerBuilder;
         }
---
+++ src/ESFA.DC.ILR.FileValidationService.Console/Stubs/FileValidationPreparationServiceStub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.FileValidationService.Service.Interface;
+
+namespace ESFA.DC.ILR.FileValidationService.Console.Stubs
+{
+    public class FileValidationPreparationServiceStub : IFileValidationPreparationService
+    {
+        public Task Prepare(IFileValidationContext fileValidationContext, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
